f402a6a234c5dee7f01d0f0e244609326f4db317
FirePlank <44502537+FirePlank@users.noreply.github.com>
2023-12-01 22:18:39 +0200

    Created some sort of basic project structure

---
+++ src/coremodules/input/__init__.py
@@ -0,0 +1,1 @@
+from src.coremodules.input.image import ImageModule
---
+++ src/coremodules/transform/rotate.py
@@ -0,0 +1,6 @@
+class RotateModule:
+    name = "Rotate"
+    tooltip = "Rotate tool for rotating the image"
+
+    def run(self):
+        pass
