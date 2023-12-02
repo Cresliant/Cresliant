bf9a880d4cd1471bc1d21530405663511dce1711
FirePlank <44502537+FirePlank@users.noreply.github.com>
2023-12-02 16:10:37 +0200

    Added poetry, build.py, fixed a lot of bugs and refactored the entire project

--- src/main.py
+++ src/main.py
@@ -1,41 +1,96 @@
+import os
+import sys
+
 import dearpygui.dearpygui as dpg
-from PIL import Image
+from pydantic import BaseModel
 
 from src.corenodes.display import InputModule, OutputModule
 from src.corenodes.transform import ResizeModule, RotateModule
 
-IMAGE_PATH = r"example.png"
-DISPLAY_TOOLTIPS: bool = False
+
+def resource_path(relative_path):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("../assets"), relative_path)
+
 
 dpg.create_context()
-img = Image.open(IMAGE_PATH)
-width, height, channels, data = dpg.load_image(IMAGE_PATH)
+dpg.create_viewport(title="Cresliant", width=1200, height=1000)
 
-dpg.create_viewport(title="CRESCIAL", width=1200, height=1000)
+# For quick testing, load an image immediately from the disk to speed up development
+width, height, channels, data = dpg.load_image(resource_path("example.png"))
+# width, height, channels, data = 300, 300, 4, [0] * 300 * 300 * 4
 
 with dpg.texture_registry():
-    texture_id = dpg.add_static_texture(width, height, data)  # Create image texture
+    dpg.add_dynamic_texture(width, height, data, tag="input_image")
+
+with dpg.font_registry():
+    dpg.add_font(resource_path("Roboto-Regular.ttf"), 17, tag="font")
+    dpg.bind_font("font")
+
 
 File_name_text = "Image_Edit"
 modules = [
-    InputModule(texture_id),
-    ResizeModule(img),
-    RotateModule(img),
-    OutputModule(texture_id),
+    InputModule("input_image", width, height),
+    ResizeModule(width, height),
+    RotateModule(),
+    OutputModule("input_image"),
 ]
 
 
-def close(sender, app_data, user_data):
-    """This close the application"""
-    dpg.stop_dearpygui()
+class Link(BaseModel):
+    source: int
+    target: int
+    id: int
+
+
+links = []
 
 
 def hide_sidebar(sender, app_data, user_data):
     pass
 
 
+def link_callback(sender, app_data):
+    link = dpg.add_node_link(app_data[0], app_data[1], parent=sender)
+    for link_ in links:
+        if link_.source == app_data[0]:
+            dpg.delete_item(link_.id)
+            links.remove(link_)
+
+    links.append(Link(source=app_data[0], target=app_data[1], id=int(link)))
+
+
+def delink_callback(sender, app_data):
+    dpg.delete_item(app_data)
+    for link in links:
+        if link.id == app_data:
+            links.remove(link)
+            break
+
+
 with dpg.window(
-    tag="Crescial",
+    tag="popup_window", no_move=True, no_close=True, no_resize=True, no_collapse=True, show=False, label="Add Node"
+):
+    for module in modules[1:-1]:
+        dpg.add_button(label=module.name, tag=module.name + "_popup", callback=module.run)
+
+
+def handle_popup(_sender, app_data):
+    if app_data == 1 and dpg.is_item_hovered("MainNodeEditor"):
+        dpg.focus_item("popup_window")
+        dpg.show_item("popup_window")
+        dpg.set_item_pos("popup_window", dpg.get_mouse_pos(local=False))
+    else:
+        dpg.hide_item("popup_window")
+
+
+with dpg.handler_registry():
+    dpg.add_mouse_click_handler(button=1, callback=handle_popup)
+    dpg.add_mouse_release_handler(button=0, callback=handle_popup)
+
+with dpg.window(
+    tag="Cresliant",
     menubar=True,
     no_title_bar=True,
     no_move=True,
@@ -43,7 +98,6 @@ with dpg.window(
     no_collapse=True,
     no_close=True,
 ):
-    # Menu bar
     with dpg.menu_bar():
         with dpg.menu(label="File"):
             with dpg.menu(label="Filename"):
@@ -66,42 +120,31 @@ with dpg.window(
                 )
 
             dpg.add_menu_item(tag="Export_Image", label="Export image")
-            dpg.add_menu_item(tag="Close_app", label="Close app", callback=close)
+            dpg.add_menu_item(tag="Close_app", label="Close app", callback=lambda: dpg.stop_dearpygui())
 
         with dpg.menu(tag="Nodes", label="Nodes"):
-            for module in modules:
-                dpg.add_menu_item(
-                    tag=module.name, label=module.name, callback=module.run
-                )
+            for module in modules[1:]:
+                dpg.add_menu_item(tag=module.name, label=module.name, callback=module.run)
 
         with dpg.menu(tag="About", label="About"):
-            dpg.add_menu_item(
-                tag="project_info", label="Learn more about project", enabled=False
-            )
+            dpg.add_menu_item(tag="project_info", label="Learn more about project", enabled=False)
             dpg.add_menu_item(tag="App_Version", label="Crescial v0.1.0", enabled=False)
 
     dpg.add_text("Ctrl+Click to remove a link.", bullet=True)
 
     with dpg.node_editor(
         tag="MainNodeEditor",
-        callback=lambda sender, app_data: dpg.add_node_link(
-            app_data[0], app_data[1], parent=sender
-        ),
-        delink_callback=lambda sender, app_data: dpg.delete_item(app_data),
+        callback=link_callback,
+        delink_callback=delink_callback,
         minimap=True,
         minimap_location=dpg.mvNodeMiniMap_Location_TopRight,
     ):
-        InputModule(texture_id).run("src/example.png")
+        modules[0].run()
+        modules[-1].run()
 
-    # Popups / Right click on nodes -> Click on node before right click on it
-
-    #    with dpg.popup("Node_Output_Image"):
-    #        dpg.add_button(label="Delete", callback=lambda: dpg.delete_item("Node_Output_Image"))
-
-    if DISPLAY_TOOLTIPS:
-        for module in modules:
-            with dpg.tooltip(parent=module.name):
-                dpg.add_text(module.tooltip)
+    for module in modules[1:-1]:
+        with dpg.tooltip(parent=module.name):
+            dpg.add_text(module.tooltip)
 
     with dpg.tooltip(parent="project_info"):
         dpg.add_text("Crescial is a free and open source image editor.")
@@ -112,6 +155,6 @@ with dpg.window(
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.set_primary_window("Crescial", True)  # set window as primary
+dpg.set_primary_window("Cresliant", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
